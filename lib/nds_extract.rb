2c9cd84e5e0eef438245897032c3a9b2284d291b
Scott Matsuda <smatsuda2014@gmail.com>
2019-11-07 21:36:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -72,7 +72,7 @@ def movies_with_directors_set(source)
   result = []
   index = 0
   while index < source.length do
-    result = movies_with_director_key(source[index][:name], source[index][:movies])
+    result << movies_with_director_key(source[index][:name], source[index][:movies])
     index += 1
   end
   result
